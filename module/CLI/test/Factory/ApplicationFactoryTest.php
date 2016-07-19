923abdf4d2c88eb6797350e2b9bb5a4e99abc20f
Alejandro Celaya <alejandro@alejandrocelaya.com>
2016-07-19 18:28:21 +0200

    Added first tests for CLI module

---
+++ module/CLI/test/Factory/ApplicationFactoryTest.php
@@ -0,0 +1,60 @@
+<?php
+namespace ShlinkioTest\Shlink\CLI\Factory;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use Shlinkio\Shlink\CLI\Factory\ApplicationFactory;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Command\Command;
+use Zend\ServiceManager\ServiceManager;
+
+class ApplicationFactoryTest extends TestCase
+{
+    /**
+     * @var ApplicationFactory
+     */
+    protected $factory;
+
+    public function setUp()
+    {
+        $this->factory = new ApplicationFactory();
+    }
+
+    /**
+     * @test
+     */
+    public function serviceIsCreated()
+    {
+        $instance = $this->factory->__invoke($this->createServiceManager(), '');
+        $this->assertInstanceOf(Application::class, $instance);
+    }
+
+    /**
+     * @test
+     */
+    public function allCommandsWhichAreServicesAreAdded()
+    {
+        $sm = $this->createServiceManager([
+            'commands' => [
+                'foo',
+                'bar',
+                'baz',
+            ],
+        ]);
+        $sm->setService('foo', $this->prophesize(Command::class)->reveal());
+        $sm->setService('baz', $this->prophesize(Command::class)->reveal());
+
+        /** @var Application $instance */
+        $instance = $this->factory->__invoke($sm, '');
+        $this->assertInstanceOf(Application::class, $instance);
+        $this->assertCount(2, $instance->all());
+    }
+
+    protected function createServiceManager($config = [])
+    {
+        return new ServiceManager(['services' => [
+            'config' => [
+                'cli' => $config,
+            ],
+        ]]);
+    }
+}
