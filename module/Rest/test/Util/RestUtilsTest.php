6f7e4f7e7f27712d52a473af46c29cb5ba139482
Alejandro Celaya <alejandro@alejandrocelaya.com>
2016-07-31 13:18:36 +0200

    Created RestUtilsTest

---
+++ module/Rest/test/Util/RestUtilsTest.php
@@ -0,0 +1,40 @@
+<?php
+namespace ShlinkioTest\Shlink\Rest\Util;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use Shlinkio\Shlink\Common\Exception\InvalidArgumentException;
+use Shlinkio\Shlink\Common\Exception\WrongIpException;
+use Shlinkio\Shlink\Core\Exception\InvalidShortCodeException;
+use Shlinkio\Shlink\Core\Exception\InvalidUrlException;
+use Shlinkio\Shlink\Rest\Exception\AuthenticationException;
+use Shlinkio\Shlink\Rest\Util\RestUtils;
+
+class RestUtilsTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function correctCodeIsReturnedFromException()
+    {
+        $this->assertEquals(
+            RestUtils::INVALID_SHORTCODE_ERROR,
+            RestUtils::getRestErrorCodeFromException(new InvalidShortCodeException())
+        );
+        $this->assertEquals(
+            RestUtils::INVALID_URL_ERROR,
+            RestUtils::getRestErrorCodeFromException(new InvalidUrlException())
+        );
+        $this->assertEquals(
+            RestUtils::INVALID_ARGUMENT_ERROR,
+            RestUtils::getRestErrorCodeFromException(new InvalidArgumentException())
+        );
+        $this->assertEquals(
+            RestUtils::INVALID_CREDENTIALS_ERROR,
+            RestUtils::getRestErrorCodeFromException(new AuthenticationException())
+        );
+        $this->assertEquals(
+            RestUtils::UNKNOWN_ERROR,
+            RestUtils::getRestErrorCodeFromException(new WrongIpException())
+        );
+    }
+}
