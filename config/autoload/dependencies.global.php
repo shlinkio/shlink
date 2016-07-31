d73d3049b777c51c1308fa32aff3e994d76be70a
Alejandro Celaya <alejandro@alejandrocelaya.com>
2016-07-31 16:42:19 +0200

    Removed dependency on expressive-helpers package

--- config/autoload/dependencies.global.php
+++ config/autoload/dependencies.global.php
@@ -2,7 +2,6 @@
 use Shlinkio\Shlink\Common\ErrorHandler\ContentBasedErrorHandler;
 use Zend\Expressive;
 use Zend\Expressive\Container;
-use Zend\Expressive\Helper;
 use Zend\Expressive\Router;
 use Zend\Expressive\Template;
 use Zend\Expressive\Twig;
@@ -13,15 +12,7 @@ return [
     'dependencies' => [
         'factories' => [
             Expressive\Application::class => Container\ApplicationFactory::class,
-
-            // Url helpers
-            Helper\UrlHelper::class => Helper\UrlHelperFactory::class,
-            Helper\ServerUrlMiddleware::class => Helper\ServerUrlMiddlewareFactory::class,
-            Helper\UrlHelperMiddleware::class => Helper\UrlHelperMiddlewareFactory::class,
-            Helper\ServerUrlHelper::class => InvokableFactory::class,
             Router\FastRouteRouter::class => InvokableFactory::class,
-
-            // View
             Template\TemplateRendererInterface::class => Twig\TwigRendererFactory::class,
         ],
         'aliases' => [
--- config/autoload/middleware-pipeline.global.php
+++ config/autoload/middleware-pipeline.global.php
@@ -1,17 +1,9 @@
 <?php
 use Zend\Expressive\Container\ApplicationFactory;
-use Zend\Expressive\Helper;
 
 return [
 
     'middleware_pipeline' => [
-        'always' => [
-            'middleware' => [
-                Helper\ServerUrlMiddleware::class,
-            ],
-            'priority' => 10000,
-        ],
-
         'routing' => [
             'middleware' => [
                 ApplicationFactory::ROUTING_MIDDLEWARE,
@@ -21,7 +13,6 @@ return [
 
         'post-routing' => [
             'middleware' => [
-                Helper\UrlHelperMiddleware::class,
                 ApplicationFactory::DISPATCH_MIDDLEWARE,
             ],
             'priority' => 1,
