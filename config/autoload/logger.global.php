b7f3c332e44c08dbacb859ae474967c123c6ca2a
Alejandro Celaya <alejandro@alejandrocelaya.com>
2016-08-08 11:56:19 +0200

    Created Logger factory and logger config, and added logger dependencies

--- config/autoload/logger.global.php
+++ config/autoload/logger.global.php
@@ -1,10 +1,30 @@
 <?php
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Logger;
+
 return [
 
     'logger' => [
+        'formatters' => [
+            'dashed' => [
+                'format' => '[%datetime%] %channel%.%level_name% - %message% %context%' . PHP_EOL,
+                'include_stacktraces' => true,
+            ],
+        ],
+
+        'handlers' => [
+            'rotating_file_handler' => [
+                'class' => RotatingFileHandler::class,
+                'level' => Logger::INFO,
+                'filename' => 'data/log/shlink_log.log',
+                'max_files' => 30,
+                'formatter' => 'dashed',
+            ],
+        ],
+
         'loggers' => [
             'Shlink' => [
-                'handlers' => [],
+                'handlers' => ['rotating_file_handler'],
             ],
         ],
     ],
--- module/Common/config/dependencies.config.php
+++ module/Common/config/dependencies.config.php
@@ -2,9 +2,11 @@
 use Acelaya\ZsmAnnotatedServices\Factory\V3\AnnotatedFactory;
 use Doctrine\Common\Cache\Cache;
 use Doctrine\ORM\EntityManager;
+use Psr\Log\LoggerInterface;
 use Shlinkio\Shlink\Common\ErrorHandler;
 use Shlinkio\Shlink\Common\Factory\CacheFactory;
 use Shlinkio\Shlink\Common\Factory\EntityManagerFactory;
+use Shlinkio\Shlink\Common\Factory\LoggerFactory;
 use Shlinkio\Shlink\Common\Factory\TranslatorFactory;
 use Shlinkio\Shlink\Common\Middleware\LocaleMiddleware;
 use Shlinkio\Shlink\Common\Service\IpLocationResolver;
@@ -19,10 +21,14 @@ return [
             EntityManager::class => EntityManagerFactory::class,
             GuzzleHttp\Client::class => InvokableFactory::class,
             Cache::class => CacheFactory::class,
-            IpLocationResolver::class => AnnotatedFactory::class,
+            LoggerInterface::class => LoggerFactory::class,
+            'Logger_Shlink' => LoggerFactory::class,
+
             Translator::class => TranslatorFactory::class,
             TranslatorExtension::class => AnnotatedFactory::class,
             LocaleMiddleware::class => AnnotatedFactory::class,
+
+            IpLocationResolver::class => AnnotatedFactory::class,
 
             ErrorHandler\ContentBasedErrorHandler::class => AnnotatedFactory::class,
             ErrorHandler\ErrorHandlerManager::class => ErrorHandler\ErrorHandlerManagerFactory::class,
@@ -31,6 +37,7 @@ return [
             'em' => EntityManager::class,
             'httpClient' => GuzzleHttp\Client::class,
             'translator' => Translator::class,
+            'logger' => LoggerInterface::class,
             AnnotatedFactory::CACHE_SERVICE => Cache::class,
         ],
     ],
