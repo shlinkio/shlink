7781f073522fe865bc098b9658d6f3fc37b58402
Alejandro Celaya <alejandro@alejandrocelaya.com>
2017-01-21 09:16:00 +0100

    Added local entity manager config that allows db host name to be set

--- config/autoload/entity-manager.global.php
+++ config/autoload/entity-manager.global.php
@@ -6,14 +6,10 @@ return [
             'proxies_dir' => 'data/proxies',
         ],
         'connection' => [
-            'driver' => 'pdo_mysql',
             'user' => env('DB_USER'),
             'password' => env('DB_PASSWORD'),
             'dbname' => env('DB_NAME', 'shlink'),
             'charset' => 'utf8',
-            'driverOptions' => [
-                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-            ],
         ],
     ],
 
