b4548f340139e2cde8d0e0fffba3fb0fede7db2d
Alejandro Celaya <alejandro@alejandrocelaya.com>
2017-01-22 11:07:18 +0100

    Added configs to enable fastroute cache

--- config/autoload/dependencies.global.php
+++ config/autoload/dependencies.global.php
@@ -4,19 +4,15 @@ use Zend\Expressive\Container;
 use Zend\Expressive\Router;
 use Zend\Expressive\Template;
 use Zend\Expressive\Twig;
-use Zend\ServiceManager\Factory\InvokableFactory;
 
 return [
 
     'dependencies' => [
         'factories' => [
             Expressive\Application::class => Container\ApplicationFactory::class,
-            Router\FastRouteRouter::class => InvokableFactory::class,
             Template\TemplateRendererInterface::class => Twig\TwigRendererFactory::class,
             \Twig_Environment::class => Twig\TwigEnvironmentFactory::class,
-        ],
-        'aliases' => [
-            Router\RouterInterface::class => Router\FastRouteRouter::class,
+            Router\RouterInterface::class => Router\FastRouteRouterFactory::class,
         ],
     ],
 
