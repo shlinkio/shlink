04c479148a136e5e7980250eb3c1f1bf202d311c
Alejandro Celaya <alejandro@alejandrocelaya.com>
2017-07-03 12:54:50 +0200

    Updated build script so that generated zip contains one folder

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ if [ "$#" -ne 1 ]; then
 fi
 
 version=$1
-builtcontent=$(readlink -f '../shlink_${version}_dist')
+builtcontent=$(readlink -f "../shlink_${version}_dist")
 projectdir=$(pwd)
 
 # Copy project content to temp dir
@@ -44,5 +44,5 @@ rm -rf config/autoload/{{,*.}local.php{,.dist},.gitignore}
 
 # Compressing file
 rm -f "${projectdir}"/build/shlink_${version}_dist.zip
-zip -ry "${projectdir}"/build/shlink_${version}_dist.zip .
+zip -ry "${projectdir}"/build/shlink_${version}_dist.zip "../shlink_${version}_dist"
 rm -rf "${builtcontent}"
