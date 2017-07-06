e0f18f8d1fd45709d6f8794a7a0dd786c3e89c2e
Alejandro Celaya <alejandro@alejandrocelaya.com>
2017-07-06 18:06:11 +0200

    Created InstallApplicationFactoryTest

---
+++ module/CLI/test/Factory/InstallApplicationFactoryTest.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace ShlinkioTest\Shlink\CLI\Factory;
+
+use PHPUnit\Framework\TestCase;
+use Shlinkio\Shlink\CLI\Factory\InstallApplicationFactory;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Filesystem\Filesystem;
+use Zend\ServiceManager\ServiceManager;
+
+class InstallApplicationFactoryTest extends TestCase
+{
+    /**
+     * @var InstallApplicationFactory
+     */
+    private $factory;
+
+    public function setUp()
+    {
+        $this->factory = new InstallApplicationFactory();
+    }
+
+    /**
+     * @test
+     */
+    public function serviceIsCreated()
+    {
+        $instance = $this->factory->__invoke(new ServiceManager(['services' => [
+            Filesystem::class => $this->prophesize(Filesystem::class)->reveal(),
+        ]]), '');
+
+        $this->assertInstanceOf(Application::class, $instance);
+    }
+}
