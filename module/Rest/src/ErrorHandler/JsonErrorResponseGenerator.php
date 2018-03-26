f788d6872f40ddda5c949c93bb04b1b8b96d8380
Alejandro Celaya <alejandro@alejandrocelaya.com>
2018-03-26 17:09:50 +0200

    Added infection to the build matrix

--- module/Rest/src/ErrorHandler/JsonErrorResponseGenerator.php
+++ module/Rest/src/ErrorHandler/JsonErrorResponseGenerator.php
@@ -20,7 +20,7 @@ class JsonErrorResponseGenerator implements ErrorResponseGeneratorInterface, Sta
      * @return Response
      * @throws \InvalidArgumentException
      */
-    public function __invoke($e, Request $request, Response $response)
+    public function __invoke(?\Throwable $e, Request $request, Response $response)
     {
         $status = $response->getStatusCode();
         $responsePhrase = $status < 400 ? 'Internal Server Error' : $response->getReasonPhrase();
