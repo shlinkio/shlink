63a69b05a1c8735eb91c9d4fb8752727f9785ffa
Alejandro Celaya <alejandro@alejandrocelaya.com>
2018-07-04 20:40:38 +0200

    Added Zend expressive swoole config provider to global config when present

--- config/config.php
+++ config/config.php
@@ -24,6 +24,9 @@ return (new ConfigAggregator\ConfigAggregator([
     Expressive\Router\FastRouteRouter\ConfigProvider::class,
     Expressive\Plates\ConfigProvider::class,
     Expressive\Helper\ConfigProvider::class,
+    \class_exists(Expressive\Swoole\ConfigProvider::class)
+        ? Expressive\Swoole\ConfigProvider::class
+        : new ConfigAggregator\ArrayProvider([]),
     ExpressiveErrorHandler\ConfigProvider::class,
     Common\ConfigProvider::class,
     Core\ConfigProvider::class,
