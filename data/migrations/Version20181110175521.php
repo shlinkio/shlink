4070b1e23d22114797f2ef340dd7b8bc9d24124d
Alejandro Celaya <alejandro@alejandrocelaya.com>
2018-11-10 18:59:25 +0100

    Updated user agent column in visits table to have a length of 512

---
+++ data/migrations/Version20181110175521.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace ShlinkMigrations;
+
+use Doctrine\DBAL\Schema\Column;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Schema\SchemaException;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20181110175521 extends AbstractMigration
+{
+    /**
+     * @throws SchemaException
+     */
+    public function up(Schema $schema): void
+    {
+        $this->getUserAgentColumn($schema)->setLength(512);
+    }
+
+    /**
+     * @throws SchemaException
+     */
+    public function down(Schema $schema): void
+    {
+        $this->getUserAgentColumn($schema)->setLength(256);
+    }
+
+    /**
+     * @throws SchemaException
+     */
+    private function getUserAgentColumn(Schema $schema): Column
+    {
+        return $schema->getTable('visits')->getColumn('user_agent');
+    }
+}
