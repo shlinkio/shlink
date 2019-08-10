64916dafacbf3765ab340b9bf54edccc0f73066a
Alejandro Celaya <alejandro@alejandrocelaya.com>
2019-08-10 14:16:19 +0200

    Fixed coding styles

--- module/IpGeolocation/src/ConfigProvider.php
+++ module/IpGeolocation/src/ConfigProvider.php
@@ -3,8 +3,6 @@ declare(strict_types=1);
 
 namespace Shlinkio\Shlink\IpGeolocation;
 
-use Zend\Config\Factory;
-use Zend\Stdlib\Glob;
 use function Shlinkio\Shlink\Common\loadConfigFromGlob;
 
 class ConfigProvider
