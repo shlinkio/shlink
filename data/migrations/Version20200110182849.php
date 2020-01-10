ad1334f289b74e826bb30a6b09f3247300d6e299
Alejandro Celaya <alejandro@alejandrocelaya.com>
2020-01-10 19:44:35 +0100

    Created database migration which ensures no nulls are present

---
+++ data/migrations/Version20200110182849.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ShlinkMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+use function Functional\each;
+use function Functional\partial_left;
+
+final class Version20200110182849 extends AbstractMigration
+{
+    private const DEFAULT_EMPTY_VALUE = '';
+    private const COLUMN_DEFAULTS_MAP = [
+        'visits' => [
+            'referer',
+            'user_agent',
+        ],
+        'visit_locations' => [
+            'timezone',
+            'country_code',
+            'country_name',
+            'region_name',
+            'city_name',
+        ],
+    ];
+
+    public function up(Schema $schema): void
+    {
+        each(
+            self::COLUMN_DEFAULTS_MAP,
+            fn (array $columns, string $tableName) =>
+                each($columns, partial_left([$this, 'setDefaultValueForColumnInTable'], $tableName)),
+        );
+    }
+
+    public function setDefaultValueForColumnInTable(string $tableName, string $columnName): void
+    {
+        $qb = $this->connection->createQueryBuilder();
+        $qb->update($tableName)
+           ->set($columnName, ':emptyValue')
+           ->setParameter('emptyValue', self::DEFAULT_EMPTY_VALUE)
+           ->where($qb->expr()->isNull($columnName))
+           ->execute();
+    }
+
+    public function down(Schema $schema): void
+    {
+        // No need (and no way) to undo this migration
+    }
+}
