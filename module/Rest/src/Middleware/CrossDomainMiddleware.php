0d7f9c05944cf646d6d51a646e7e69d1ca9f5bff
Alejandro Celaya <alejandro@alejandrocelaya.com>
2020-01-12 11:06:16 +0100

    Removed extra semicolon added by mistake

--- module/Rest/src/Middleware/CrossDomainMiddleware.php
+++ module/Rest/src/Middleware/CrossDomainMiddleware.php
@@ -10,7 +10,7 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Shlinkio\Shlink\Rest\Authentication;
-use Zend\Diactoros\Response\EmptyResponse;;
+use Zend\Diactoros\Response\EmptyResponse;
 use Zend\Expressive\Router\RouteResult;
 
 use function array_merge;
