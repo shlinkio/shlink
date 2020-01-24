8ec686f4e2840692391ce21c6f7a7d86f180327e
Alejandro Celaya <alejandro.celaya@adidas.com>
2020-01-24 21:19:40 +0100

    Updated order in which headers for remote IP detection are inspected

--- config/autoload/client-detection.global.php
+++ config/autoload/client-detection.global.php
@@ -7,11 +7,11 @@ return [
     'ip_address_resolution' => [
         'headers_to_inspect' => [
             'CF-Connecting-IP',
-            'True-Client-IP',
-            'X-Real-IP',
-            'Forwarded',
             'X-Forwarded-For',
             'X-Forwarded',
+            'Forwarded',
+            'True-Client-IP',
+            'X-Real-IP',
             'X-Cluster-Client-Ip',
             'Client-Ip',
         ],
