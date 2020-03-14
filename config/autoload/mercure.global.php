10cad3324828b249125f200ec553dbbb4f925eab
Alejandro Celaya <alejandro@alejandrocelaya.com>
2020-03-14 20:19:16 +0100

    Added configuration for mercure integration

---
+++ config/autoload/mercure.global.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+
+    'mercure' => [
+        'public_hub_url' => null,
+        'internal_hub_url' => null,
+        'jwt_secret' => null,
+    ],
+
+];
