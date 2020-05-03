867659ea25d6e9ed79ea368c3af2090f4204e412
Alejandro Celaya <alejandro@alejandrocelaya.com>
2020-05-03 19:15:26 +0200

    Created index on visits.date column

---
+++ data/migrations/Version20200503170404.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ShlinkMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20200503170404 extends AbstractMigration
+{
+    private const INDEX_NAME = 'IDX_visits_date';
+
+    public function up(Schema $schema): void
+    {
+        $visits = $schema->getTable('visits');
+        $this->skipIf($visits->hasIndex(self::INDEX_NAME));
+        $visits->addIndex(['date'], self::INDEX_NAME);
+    }
+
+    public function down(Schema $schema): void
+    {
+        $visits = $schema->getTable('visits');
+        $this->skipIf(! $visits->hasIndex(self::INDEX_NAME));
+        $visits->dropIndex(self::INDEX_NAME);
+    }
+}
