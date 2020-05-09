e1e3c7f0614e456f76ab75910569386273fcb2b0
Alejandro Celaya <alejandro@alejandrocelaya.com>
2020-05-09 10:10:48 +0200

    Created paginator adapter tests

--- module/Core/src/Paginator/Adapter/VisitsForTagPaginatorAdapter.php
+++ module/Core/src/Paginator/Adapter/VisitsForTagPaginatorAdapter.php
@@ -4,17 +4,14 @@ declare(strict_types=1);
 
 namespace Shlinkio\Shlink\Core\Paginator\Adapter;
 
-use Laminas\Paginator\Adapter\AdapterInterface;
 use Shlinkio\Shlink\Core\Model\VisitsParams;
 use Shlinkio\Shlink\Core\Repository\VisitRepositoryInterface;
 
-class VisitsForTagPaginatorAdapter implements AdapterInterface
+class VisitsForTagPaginatorAdapter extends AbstractCacheableCountPaginatorAdapter
 {
     private VisitRepositoryInterface $visitRepository;
     private string $tag;
     private VisitsParams $params;
-
-    private ?int $count = null;
 
     public function __construct(VisitRepositoryInterface $visitRepository, string $tag, VisitsParams $params)
     {
@@ -33,18 +30,8 @@ class VisitsForTagPaginatorAdapter implements AdapterInterface
         );
     }
 
-    public function count(): int
+    protected function doCount(): int
     {
-        // Since a new adapter instance is created every time visits are fetched, it is reasonably safe to internally
-        // cache the count value.
-        // The reason it is cached is because the Paginator is actually calling the method twice.
-        // An inconsistent value could be returned if between the first call and the second one, a new visit is created.
-        // However, it's almost instant, and then the adapter instance is discarded immediately after.
-
-        if ($this->count !== null) {
-            return $this->count;
-        }
-
-        return $this->count = $this->visitRepository->countVisitsByTag($this->tag, $this->params->getDateRange());
+        return $this->visitRepository->countVisitsByTag($this->tag, $this->params->getDateRange());
     }
 }
--- module/Core/src/Paginator/Adapter/VisitsPaginatorAdapter.php
+++ module/Core/src/Paginator/Adapter/VisitsPaginatorAdapter.php
@@ -4,18 +4,15 @@ declare(strict_types=1);
 
 namespace Shlinkio\Shlink\Core\Paginator\Adapter;
 
-use Laminas\Paginator\Adapter\AdapterInterface;
 use Shlinkio\Shlink\Core\Model\ShortUrlIdentifier;
 use Shlinkio\Shlink\Core\Model\VisitsParams;
 use Shlinkio\Shlink\Core\Repository\VisitRepositoryInterface;
 
-class VisitsPaginatorAdapter implements AdapterInterface
+class VisitsPaginatorAdapter extends AbstractCacheableCountPaginatorAdapter
 {
     private VisitRepositoryInterface $visitRepository;
     private ShortUrlIdentifier $identifier;
     private VisitsParams $params;
-
-    private ?int $count = null;
 
     public function __construct(
         VisitRepositoryInterface $visitRepository,
@@ -38,19 +35,9 @@ class VisitsPaginatorAdapter implements AdapterInterface
         );
     }
 
-    public function count(): int
+    protected function doCount(): int
     {
-        // Since a new adapter instance is created every time visits are fetched, it is reasonably safe to internally
-        // cache the count value.
-        // The reason it is cached is because the Paginator is actually calling the method twice.
-        // An inconsistent value could be returned if between the first call and the second one, a new visit is created.
-        // However, it's almost instant, and then the adapter instance is discarded immediately after.
-
-        if ($this->count !== null) {
-            return $this->count;
-        }
-
-        return $this->count = $this->visitRepository->countVisitsByShortCode(
+        return $this->visitRepository->countVisitsByShortCode(
             $this->identifier->shortCode(),
             $this->identifier->domain(),
             $this->params->getDateRange(),
