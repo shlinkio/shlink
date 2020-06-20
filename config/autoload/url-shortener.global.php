f2f07be11f31bda6ad3da436d0a3eb013ffadd97
Alejandro Celaya <alejandro@alejandrocelaya.com>
2020-06-20 11:07:15 +0200

    Updated to latest installer, supporting redirects customizations

--- config/autoload/url-shortener.global.php
+++ config/autoload/url-shortener.global.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use const Shlinkio\Shlink\Core\DEFAULT_REDIRECT_CACHE_LIFETIME;
 use const Shlinkio\Shlink\Core\DEFAULT_REDIRECT_STATUS_CODE;
 use const Shlinkio\Shlink\Core\DEFAULT_SHORT_CODES_LENGTH;
 
@@ -17,7 +18,7 @@ return [
         'visits_webhooks' => [],
         'default_short_codes_length' => DEFAULT_SHORT_CODES_LENGTH,
         'redirect_status_code' => DEFAULT_REDIRECT_STATUS_CODE,
-        'redirect_cache_lifetime' => 30,
+        'redirect_cache_lifetime' => DEFAULT_REDIRECT_CACHE_LIFETIME,
     ],
 
 ];
