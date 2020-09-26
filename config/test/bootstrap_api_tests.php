cc57dcd01a1ffbd7e329f49a124ff3cba4cb99f1
Alejandro Celaya <alejandro@alejandrocelaya.com>
2020-09-26 10:43:50 +0200

    Added code coverage to API tests

--- config/test/bootstrap_api_tests.php
+++ config/test/bootstrap_api_tests.php
@@ -7,12 +7,28 @@ namespace Shlinkio\Shlink\TestUtils;
 use Doctrine\ORM\EntityManager;
 use Psr\Container\ContainerInterface;
 
+use function register_shutdown_function;
+use function sprintf;
+
+use const ShlinkioTest\Shlink\SWOOLE_TESTING_HOST;
+use const ShlinkioTest\Shlink\SWOOLE_TESTING_PORT;
+
 /** @var ContainerInterface $container */
 $container = require __DIR__ . '/../container.php';
 $testHelper = $container->get(Helper\TestHelper::class);
 $config = $container->get('config');
 $em = $container->get(EntityManager::class);
+$httpClient = $container->get('shlink_test_api_client');
+
+// Start code coverage collecting on swoole process, and stop it when process shuts down
+$httpClient->request('GET', sprintf('http://%s:%s/api-tests/start-coverage', SWOOLE_TESTING_HOST, SWOOLE_TESTING_PORT));
+register_shutdown_function(function () use ($httpClient): void {
+    $httpClient->request(
+        'GET',
+        sprintf('http://%s:%s/api-tests/stop-coverage', SWOOLE_TESTING_HOST, SWOOLE_TESTING_PORT),
+    );
+});
 
 $testHelper->createTestDb();
-ApiTest\ApiTestCase::setApiClient($container->get('shlink_test_api_client'));
+ApiTest\ApiTestCase::setApiClient($httpClient);
 ApiTest\ApiTestCase::setSeedFixturesCallback(fn () => $testHelper->seedFixtures($em, $config['data_fixtures'] ?? []));
