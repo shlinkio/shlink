55ea8a6912e1ff7e1587e9d684cdfbad63174d14
Alejandro Celaya <alejandrocelaya@gmail.com>
2020-12-02 12:00:05 +0100

    #896 Added support for unicode characters in custom slugs

--- module/Core/functions/functions.php
+++ module/Core/functions/functions.php
@@ -18,7 +18,7 @@ const MIN_SHORT_CODES_LENGTH = 4;
 const DEFAULT_REDIRECT_STATUS_CODE = StatusCodeInterface::STATUS_FOUND;
 const DEFAULT_REDIRECT_CACHE_LIFETIME = 30;
 const LOCAL_LOCK_FACTORY = 'Shlinkio\Shlink\LocalLockFactory';
-const CUSTOM_SLUGS_REGEXP = '/[^A-Za-z0-9._~]+/';
+const CUSTOM_SLUGS_REGEXP = '/[^\pL\pN._~]/u'; // Any unicode word or number, plus ".", "_" and "~" chars
 
 function generateRandomShortCode(int $length): string
 {
