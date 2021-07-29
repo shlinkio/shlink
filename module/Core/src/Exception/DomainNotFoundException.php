2ac7be4363f7e86ad2061da2dca557afa3c51bc5
Alejandro Celaya <alejandro@alejandrocelaya.com>
2021-07-29 18:23:41 +0200

    Extended DomainNotFoundException to allow creating from an authority

--- module/Core/src/Exception/DomainNotFoundException.php
+++ module/Core/src/Exception/DomainNotFoundException.php
@@ -17,16 +17,27 @@ class DomainNotFoundException extends DomainException implements ProblemDetailsE
     private const TITLE = 'Domain not found';
     private const TYPE = 'DOMAIN_NOT_FOUND';
 
+    private function __construct(string $message, array $additional)
+    {
+        parent::__construct($message);
+
+        $this->detail = $message;
+        $this->title = self::TITLE;
+        $this->type = self::TYPE;
+        $this->status = StatusCodeInterface::STATUS_NOT_FOUND;
+        $this->additional = $additional;
+    }
+
     public static function fromId(string $id): self
     {
-        $e = new self(sprintf('Domain with id "%s" could not be found', $id));
+        return new self(sprintf('Domain with id "%s" could not be found', $id), ['id' => $id]);
+    }
 
-        $e->detail = $e->getMessage();
-        $e->title = self::TITLE;
-        $e->type = self::TYPE;
-        $e->status = StatusCodeInterface::STATUS_NOT_FOUND;
-        $e->additional = ['id' => $id];
-
-        return $e;
+    public static function fromAuthority(string $authority): self
+    {
+        return new self(
+            sprintf('Domain with authority "%s" could not be found', $authority),
+            ['authority' => $authority],
+        );
     }
 }
