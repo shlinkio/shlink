bd3bb67949d366fde8609f17fc7a2f9b7aa7f18d
Alejandro Celaya <alejandro@alejandrocelaya.com>
2021-12-11 17:07:40 +0100

    Added dependencies and config to integrate with Rabbit MQ

---
+++ config/autoload/rabbit.global.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+use Laminas\ServiceManager\AbstractFactory\ConfigAbstractFactory;
+use Laminas\ServiceManager\Proxy\LazyServiceFactory;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+use function Shlinkio\Shlink\Common\env;
+
+return [
+
+    'rabbit' => [
+        'host' => env('RABBITMQ_HOST'),
+        'port' => env('RABBITMQ_PORT', '5672'),
+        'user' => env('RABBITMQ_USER'),
+        'password' => env('RABBITMQ_PASSWORD'),
+        'vhost' => env('RABBITMQ_VHOST', '/'),
+        'exchange' => env('RABBITMQ_EXCHANGE', 'shlink-exchange'),
+        'queue' => env('RABBITMQ_QUEUE', 'shlink-queue'),
+    ],
+
+    'dependencies' => [
+        'factories' => [
+            AMQPStreamConnection::class => ConfigAbstractFactory::class,
+        ],
+        'delegators' => [
+            AMQPStreamConnection::class => [
+                LazyServiceFactory::class,
+            ],
+        ],
+        'lazy_services' => [
+            'class_map' => [
+                AMQPStreamConnection::class => AMQPStreamConnection::class,
+            ],
+        ],
+    ],
+
+    ConfigAbstractFactory::class => [
+        AMQPStreamConnection::class => [
+            'config.rabbit.host',
+            'config.rabbit.port',
+            'config.rabbit.user',
+            'config.rabbit.password',
+            'config.rabbit.vhost',
+        ],
+    ],
+
+];
