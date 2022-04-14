52c55f385da9ef73a05e19b1ee292d8c1378baee
Alejandro Celaya <alejandrocelaya@gmail.com>
2022-04-14 14:22:48 +0200

    Added support to set the timezone via config/env vars

--- config/container.php
+++ config/container.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Laminas\ServiceManager\ServiceManager;
+use Shlinkio\Shlink\Core\Config\EnvVars;
 use Symfony\Component\Lock;
 
 use const Shlinkio\Shlink\LOCAL_LOCK_FACTORY;
@@ -11,6 +12,9 @@ chdir(dirname(__DIR__));
 
 require 'vendor/autoload.php';
 
+// This is one of the first files loaded. Configure the timezone here
+date_default_timezone_set(EnvVars::TIMEZONE()->loadFromEnv(date_default_timezone_get()));
+
 // This class alias tricks the ConfigAbstractFactory to return Lock\Factory instances even with a different service name
 // It needs to be placed here as individual config files will not be loaded once config is cached
 if (! class_exists(LOCAL_LOCK_FACTORY)) {
@@ -18,7 +22,7 @@ if (! class_exists(LOCAL_LOCK_FACTORY)) {
 }
 
 // Build container
-return (function () {
+return (static function () {
     $config = require __DIR__ . '/config.php';
     $container = new ServiceManager($config['dependencies']);
     $container->setService('config', $config);
