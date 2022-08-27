c5b6d203f53698437623c47d262ac95e5ee43e3b
Alejandro Celaya <alejandrocelaya@gmail.com>
2022-08-27 08:01:57 +0200

    Simplified RoadRunner worker, and fixed RoadRunner reloading config

--- bin/roadrunner-worker.php
+++ bin/roadrunner-worker.php
@@ -4,27 +4,20 @@ declare(strict_types=1);
 
 use Mezzio\Application;
 use Psr\Container\ContainerInterface;
-use Psr\Http\Message\ServerRequestFactoryInterface;
-use Psr\Http\Message\StreamFactoryInterface;
-use Psr\Http\Message\UploadedFileFactoryInterface;
 use Shlinkio\Shlink\EventDispatcher\RoadRunner\RoadRunnerTaskConsumerToListener;
 use Spiral\RoadRunner\Http\PSR7Worker;
-use Spiral\RoadRunner\Worker;
+
+use function Shlinkio\Shlink\Config\env;
 
 (static function (): void {
-    $rrMode = getenv('RR_MODE');
     /** @var ContainerInterface $container */
     $container = include __DIR__ . '/../config/container.php';
+    $rrMode = env('RR_MODE');
 
     if ($rrMode === 'http') {
         // This was spin-up as a web worker
         $app = $container->get(Application::class);
-        $worker = new PSR7Worker(
-            Worker::create(),
-            $container->get(ServerRequestFactoryInterface::class),
-            $container->get(StreamFactoryInterface::class),
-            $container->get(UploadedFileFactoryInterface::class),
-        );
+        $worker = $container->get(PSR7Worker::class);
 
         while ($req = $worker->waitRequest()) {
             try {
--- config/autoload/dependencies.global.php
+++ config/autoload/dependencies.global.php
@@ -3,12 +3,22 @@
 declare(strict_types=1);
 
 use GuzzleHttp\Client;
+use Laminas\ServiceManager\AbstractFactory\ConfigAbstractFactory;
 use Mezzio\Container;
 use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\ServerRequestFactoryInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\UploadedFileFactoryInterface;
+use Spiral\RoadRunner\Http\PSR7Worker;
+use Spiral\RoadRunner\WorkerInterface;
 
 return [
 
     'dependencies' => [
+        'factories' => [
+            PSR7Worker::class => ConfigAbstractFactory::class,
+        ],
+
         'delegators' => [
             Mezzio\Application::class => [
                 Container\ApplicationConfigInjectionDelegator::class,
@@ -23,6 +33,15 @@ return [
             'proxies_target_dir' => 'data/proxies',
             'proxies_namespace' => 'ShlinkProxy',
             'write_proxy_files' => true,
+        ],
+    ],
+
+    ConfigAbstractFactory::class => [
+        PSR7Worker::class => [
+            WorkerInterface::class,
+            ServerRequestFactoryInterface::class,
+            StreamFactoryInterface::class,
+            UploadedFileFactoryInterface::class,
         ],
     ],
 
