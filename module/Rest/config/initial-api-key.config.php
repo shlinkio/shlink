0e54ed691d2a5b6f81824e8289f15874eb13eaf5
Alejandro Celaya <alejandrocelaya@gmail.com>
2022-09-11 12:10:21 +0200

    Created InitialApiKeyDelegatorTest

--- module/Rest/config/initial-api-key.config.php
+++ module/Rest/config/initial-api-key.config.php
@@ -11,6 +11,8 @@ use const PHP_SAPI;
 
 return [
 
+    // We will try to load the initial API key only for openswoole and RoadRunner.
+    // For php-fpm, the check against the database would happen on every request, resulting in a very bad performance.
     'initial_api_key' => PHP_SAPI !== 'cli' ? null : EnvVars::INITIAL_API_KEY->loadFromEnv(),
 
     'dependencies' => [
