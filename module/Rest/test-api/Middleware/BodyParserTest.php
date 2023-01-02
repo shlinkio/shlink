112cbb9039b1facd29251158bb266477d332df6a
Alejandro Celaya <alejandrocelaya@gmail.com>
2023-01-02 13:38:04 +0100

    Added API test for malformed request JSON body

---
+++ module/Rest/test-api/Middleware/BodyParserTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ShlinkioApiTest\Shlink\Rest\Middleware;
+
+use GuzzleHttp\RequestOptions;
+use Shlinkio\Shlink\TestUtils\ApiTest\ApiTestCase;
+
+class BodyParserTest extends ApiTestCase
+{
+    /** @test */
+    public function returnsErrorWhenRequestBodyIsInvalidJson(): void
+    {
+        $resp = $this->callApiWithKey(self::METHOD_POST, '/short-urls', [
+            RequestOptions::HEADERS => ['content-type' => 'application/json'],
+            RequestOptions::BODY => '{"foo',
+        ]);
+        $payload = $this->getJsonResponsePayload($resp);
+
+        self::assertEquals(400, $resp->getStatusCode());
+        self::assertEquals(400, $payload['status']);
+        self::assertEquals('Provided request does not contain a valid JSON body.', $payload['detail']);
+        self::assertEquals('Malformed request body', $payload['title']);
+        self::assertEquals('https://shlink.io/api/error/malformed-request-body', $payload['type']);
+    }
+}
