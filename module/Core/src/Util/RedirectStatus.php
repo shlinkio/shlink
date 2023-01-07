edaf999bf522c3f08b4ddd1065128e13ae83420b
Alejandro Celaya <alejandrocelaya@gmail.com>
2023-01-07 17:09:53 +0100

    Fixed constant assignment on enum which is not valid for PHP 8.1

--- module/Core/src/Util/RedirectStatus.php
+++ module/Core/src/Util/RedirectStatus.php
@@ -2,16 +2,14 @@
 
 namespace Shlinkio\Shlink\Core\Util;
 
-use Fig\Http\Message\StatusCodeInterface;
-
 use function Functional\contains;
 
 enum RedirectStatus: int
 {
-    case STATUS_301 = StatusCodeInterface::STATUS_MOVED_PERMANENTLY;
-    case STATUS_302 = StatusCodeInterface::STATUS_FOUND;
-    case STATUS_307 = StatusCodeInterface::STATUS_TEMPORARY_REDIRECT;
-    case STATUS_308 = StatusCodeInterface::STATUS_PERMANENT_REDIRECT;
+    case STATUS_301 = 301; // StatusCodeInterface::STATUS_MOVED_PERMANENTLY;
+    case STATUS_302 = 302; // StatusCodeInterface::STATUS_FOUND;
+    case STATUS_307 = 307; // StatusCodeInterface::STATUS_TEMPORARY_REDIRECT;
+    case STATUS_308 = 308; // StatusCodeInterface::STATUS_PERMANENT_REDIRECT;
 
     public function allowsCache(): bool
     {
