8afa582aa5ee869f041d6002bb58c57a94aeb6da
Alejandro Celaya <alejandrocelaya@gmail.com>
2023-01-29 11:32:13 +0100

    Create ShortUrlModeTest

---
+++ module/Core/test/ShortUrl/Model/ShortUrlModeTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ShlinkioTest\Shlink\Core\ShortUrl\Model;
+
+use PHPUnit\Framework\TestCase;
+use Shlinkio\Shlink\Core\ShortUrl\Model\ShortUrlMode;
+
+class ShortUrlModeTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider provideModes
+     */
+    public function deprecatedValuesAreProperlyParsed(string $mode, ?ShortUrlMode $expected): void
+    {
+        self::assertSame($expected, ShortUrlMode::tryDeprecated($mode));
+    }
+
+    public function provideModes(): iterable
+    {
+        yield 'invalid' => ['invalid', null];
+        yield 'foo' => ['foo', null];
+        yield 'loose' => ['loose', ShortUrlMode::LOOSE];
+        yield 'loosely' => ['loosely', ShortUrlMode::LOOSE];
+        yield 'strict' => ['strict', ShortUrlMode::STRICT];
+    }
+}
