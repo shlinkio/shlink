790dcebe0230bb54f18cdc1f54a4ce076d805581
Aliaksandr Hinko <aliaksandr.gg@gmail.com>
2023-01-30 19:33:44 +0300

    SWR-8265. Server: Short URL Migration.

--- module/CLI/src/Command/Import/DataImportCommand.php
+++ module/CLI/src/Command/Import/DataImportCommand.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Shlinkio\Shlink\CLI\Command\Import;
 
 use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\DBAL\Connection;
 use Doctrine\ORM\EntityManagerInterface;
 use League\Csv\Reader;
 use Shlinkio\Shlink\CLI\Util\ExitCodes;
@@ -19,8 +20,10 @@ use Shlinkio\Shlink\Rest\Entity\ApiKey;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+
 use function Functional\reduce_left;
 
 final class DataImportCommand extends Command
@@ -29,7 +32,7 @@ final class DataImportCommand extends Command
 
     private const SHORT_URL_BATCH_SIZE = 250;
     private const VISITS_BATCH_SIZE = 500;
-    private const DEFAULT_DELIMITER = ';';
+    private const DEFAULT_DELIMITER = ',';
 
     private ?SymfonyStyle $io = null;
 
@@ -45,11 +48,12 @@ final class DataImportCommand extends Command
     {
         $this
             ->setName(self::NAME)
-            ->setDescription('Import clicks from CSV file')
+            ->setDescription('Import data from CSV file')
             ->addArgument('path', InputArgument::REQUIRED, 'CSV file path')
-            ->addArgument('domain', InputArgument::REQUIRED, 'Domain for searching')
-            ->addArgument('startFromCode', InputArgument::OPTIONAL, 'Import will start from this short code')
-            ->addArgument('delimiter', InputArgument::OPTIONAL, 'CSV delimiter', self::DEFAULT_DELIMITER);
+            ->addOption('domain', null, InputOption::VALUE_REQUIRED)
+            ->addOption('delimiter', null, InputOption::VALUE_OPTIONAL)
+            ->addOption('length', null, InputOption::VALUE_OPTIONAL)
+            ->addOption('from', null, InputOption::VALUE_OPTIONAL);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -66,8 +70,8 @@ final class DataImportCommand extends Command
             return ExitCodes::EXIT_FAILURE;
         }
 
-        $startFromShortCode = $input->getArgument('startFromCode');
-        $domainName = $input->getArgument('domain');
+        $startFromShortCode = $input->getOption('from');
+        $domainName = $input->getOption('domain');
         $domain = $this->getDomain($domainName);
         $apiKeyId = $this->em->getRepository(ApiKey::class)->findOneBy([
             'enabled' => true,
@@ -77,6 +81,13 @@ final class DataImportCommand extends Command
             return ExitCodes::EXIT_FAILURE;
         }
 
+        $processedData = [
+            'shortUrls' => 0,
+            'visits' => 0,
+            'tags' => 0,
+        ];
+        $codeLength = $input->getOption('length');
+        $skippedInvalidShortCodes = [];
         $shortUrlInserts = [];
         $shortUrlWithClicks = [];
         $tags = [];
@@ -86,15 +97,17 @@ final class DataImportCommand extends Command
                 if ($startFromShortCode && $shortUrlData->shortCode !== $startFromShortCode) {
                     continue;
                 }
+                if ($codeLength && mb_strlen($shortUrlData->shortCode) < $codeLength) {
+                    $skippedInvalidShortCodes[] = $shortUrlData->shortCode;
+                    continue;
+                }
 
                 ++$iteration;
                 $this->io->writeln([
                     sprintf('Iteration: <info>%d</info>', $iteration),
-                    sprintf('Short code: <info>%s</info>', $shortUrlData->shortCode),
-                    sprintf('Clicks: <info>%s</info>' . PHP_EOL, $shortUrlData->visitsCount),
                 ]);
 
-                $shortUrlInserts[] = [
+                $shortUrlInserts[$shortUrlData->shortCode] = [
                     'domain_id' => $domain->getId(),
                     'author_api_key_id' => $apiKeyId,
                     'original_url' => $shortUrlData->longUrl,
@@ -115,15 +128,22 @@ final class DataImportCommand extends Command
                     continue;
                 }
 
-                $this->processData($shortUrlInserts, $tags, $shortUrlWithClicks, $domain);
+                $this->processData($shortUrlInserts, $tags, $shortUrlWithClicks, $domain, $processedData);
                 $shortUrlInserts = [];
                 $tags = [];
                 $shortUrlWithClicks = [];
             }
 
             if ($shortUrlInserts) {
-                $this->processData($shortUrlInserts, $tags, $shortUrlWithClicks, $domain);
+                $this->processData($shortUrlInserts, $tags, $shortUrlWithClicks, $domain, $processedData);
             }
+            if ($skippedInvalidShortCodes) {
+                $this->io->warning(sprintf(
+                    'The following short codes were skipped: %s',
+                    implode(', ', $skippedInvalidShortCodes)
+                ));
+            }
+            $this->io->info(print_r($processedData, true));
         } catch (\Throwable $e) {
             $this->io->error([
                 sprintf('Message: %s', $e->getMessage()),
@@ -135,19 +155,21 @@ final class DataImportCommand extends Command
         return ExitCodes::EXIT_SUCCESS;
     }
 
-    private function processData(array $inserts, array $tags, array $shortUrlWithClicks, Domain $domain)
+    private function processData(array &$inserts, array &$tags, array &$shortUrlWithClicks, Domain $domain, array &$processedData)
     {
         $this->io->info('Batch processing...');
         $this->em->getConnection()->beginTransaction();
         try {
+            $this->skipExisting($inserts, $domain);
             $this->runBatchInserts('short_urls', $inserts);
-            $this->io->info('Short urls processed - ' . count($inserts));
+            $processedData['shortUrls'] += count($inserts);
+            $this->io->info('Short URLs were processed');
 
-            $this->processTags($tags, $domain);
-            $this->io->info('Tags processed - ' . count($tags));
+            $this->processTags($tags, $domain, $processedData);
+            $this->io->info('Tags were processed');
 
-            $this->processClicks($shortUrlWithClicks, $domain);
-            $this->io->info('Visits processed');
+            $this->processClicks($shortUrlWithClicks, $domain, $processedData);
+            $this->io->info('Visits were processed');
 
             $this->em->getConnection()->commit();
         } catch (\Throwable $e) {
@@ -158,7 +180,7 @@ final class DataImportCommand extends Command
         $this->em->clear();
     }
 
-    private function processTags(array $tagsListing, Domain $domain)
+    private function processTags(array &$tagsListing, Domain $domain, array &$processedData)
     {
         foreach ($tagsListing as $shortCode => $tags) {
             $shortUrl = $this->getShortUrl($shortCode, (int) $domain->getId());
@@ -173,12 +195,13 @@ final class DataImportCommand extends Command
             }, $tags));
             $shortUrl->setTags($tagEntities);
             $this->em->persist($shortUrl);
+            $processedData['tags'] += count($tagEntities);
         }
         $this->em->flush();
         $this->em->clear();
     }
 
-    private function processClicks(array $shortUrlWithClicks, Domain $domain): void
+    private function processClicks(array &$shortUrlWithClicks, Domain $domain, array &$processedData): void
     {
         $visitInserts = [];
         /**
@@ -201,11 +224,28 @@ final class DataImportCommand extends Command
                     continue;
                 }
                 $this->runBatchInserts('visits', $visitInserts);
+                $processedData['visits'] += count($visitInserts);
                 $visitInserts = [];
             }
         }
         if ($visitInserts) {
             $this->runBatchInserts('visits', $visitInserts);
+        }
+    }
+
+    private function skipExisting(array &$shortUrlsInserts, Domain $domain): void
+    {
+        $shortCodes = array_keys($shortUrlsInserts);
+        $existingShortUrls = $this->em
+            ->getConnection()
+            ->executeQuery(
+                'SELECT short_code FROM short_urls WHERE short_code IN (?) AND domain_id=?',
+                [$shortCodes, (int) $domain->getId()],
+                [Connection::PARAM_STR_ARRAY, \PDO::PARAM_INT]
+            )
+            ->fetchAllAssociative();
+        foreach ($existingShortUrls as $existingShortUrl) {
+            unset($shortUrlsInserts[$existingShortUrl['short_code']]);
         }
     }
 
@@ -215,9 +255,12 @@ final class DataImportCommand extends Command
      * @return void
      * @throws \Doctrine\DBAL\Exception
      */
-    private function runBatchInserts(string $tableName, array $batchInserts): void
+    private function runBatchInserts(string $tableName, array &$batchInserts): void
     {
-        $columns = implode(',', array_keys($batchInserts[0]));
+        if (!$batchInserts) {
+            return;
+        }
+        $columns = implode(',', array_keys(array_values($batchInserts)[0]));
         $params = [];
         $batchValues = '';
         foreach ($batchInserts as $insertItem) {
@@ -255,8 +298,8 @@ final class DataImportCommand extends Command
      */
     private function records($file, InputInterface $input): iterable
     {
-        $delimiter = $input->getArgument('delimiter') ?: self::DEFAULT_DELIMITER;
-        $domain = $input->getArgument('domain');
+        $delimiter = $input->getOption('delimiter') ?: self::DEFAULT_DELIMITER;
+        $domain = $input->getOption('domain');
 
         $csvReader = Reader::createFromStream($file)
             ->setDelimiter($delimiter)
@@ -264,9 +307,7 @@ final class DataImportCommand extends Command
 
         foreach ($csvReader as $record) {
             $record = $this->remapRecordHeaders($record);
-
-            // TODO "," ?
-            $tags = array_filter(explode('|', (string) $record['tags']), fn (string $tag) => trim($tag));
+            $tags = array_filter(explode(',', (string) ($record['tags'] ?? '')), fn (string $tag) => trim($tag));
 
             yield new ImportedShlinkUrl(
                 ImportSource::CSV,
