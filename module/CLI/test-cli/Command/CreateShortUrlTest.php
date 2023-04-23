1b8334499598d20383abb0f523b6ea081ad7bdaa
Alejandro Celaya <alejandrocelaya@gmail.com>
2023-04-23 11:20:54 +0200

    Create CLI test checking default domain is ignored even if explicitly provided

---
+++ module/CLI/test-cli/Command/CreateShortUrlTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ShlinkioCliTest\Shlink\CLI\Command;
+
+use PHPUnit\Framework\Attributes\Test;
+use Shlinkio\Shlink\CLI\Command\ShortUrl\CreateShortUrlCommand;
+use Shlinkio\Shlink\CLI\Command\ShortUrl\ListShortUrlsCommand;
+use Shlinkio\Shlink\CLI\Util\ExitCodes;
+use Shlinkio\Shlink\TestUtils\CliTest\CliTestCase;
+
+class CreateShortUrlTest extends CliTestCase
+{
+    #[Test]
+    public function defaultDomainIsIgnoredWhenExplicitlyProvided(): void
+    {
+        $slug = 'testing-default-domain';
+        $defaultDomain = 's.test';
+
+        [$output, $exitCode] = $this->exec(
+            [CreateShortUrlCommand::NAME, 'https://example.com', '--domain', $defaultDomain, '--custom-slug', $slug],
+        );
+
+        self::assertEquals(ExitCodes::EXIT_SUCCESS, $exitCode);
+        self::assertStringContainsString('Generated short URL: http://' . $defaultDomain . '/' . $slug, $output);
+
+        [$listOutput] = $this->exec([ListShortUrlsCommand::NAME, '--show-domain', '--search-term', $slug]);
+        self::assertStringContainsString('DEFAULT', $listOutput);
+    }
+}
