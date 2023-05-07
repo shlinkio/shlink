a797b74a70e1e4f199f6a1776705eb2aac98469a
Alejandro Celaya <alejandrocelaya@gmail.com>
2023-05-07 13:18:19 +0200

    Standardize logger for all Shlink execution contexts

--- docker/config/shlink_in_docker.local.php
+++ docker/config/shlink_in_docker.local.php
@@ -6,14 +6,12 @@ namespace Shlinkio\Shlink;
 
 use Shlinkio\Shlink\Common\Logger\LoggerType;
 
-use function Shlinkio\Shlink\Config\runningInRoadRunner;
-
 return [
 
     'logger' => [
         'Shlink' => [
             'type' => LoggerType::STREAM->value,
-            'destination' => runningInRoadRunner() ? 'php://stderr' : 'php://stdout',
+            'destination' => 'php://stderr',
         ],
     ],
 
