69ff7de481e8c4878c62df345214068923002b3a
Alejandro Celaya <alejandrocelaya@gmail.com>
2023-05-14 12:32:54 +0200

    Create ShortUrlVisitsDeleterTest

--- module/Core/src/ShortUrl/ShortUrlVisitsDeleter.php
+++ module/Core/src/ShortUrl/ShortUrlVisitsDeleter.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Shlinkio\Shlink\Core\ShortUrl;
 
+use Shlinkio\Shlink\Core\Exception\ShortUrlNotFoundException;
 use Shlinkio\Shlink\Core\Model\BulkDeleteResult;
 use Shlinkio\Shlink\Core\ShortUrl\Model\ShortUrlIdentifier;
 use Shlinkio\Shlink\Core\Visit\Repository\VisitDeleterRepositoryInterface;
@@ -17,6 +18,9 @@ class ShortUrlVisitsDeleter implements ShortUrlVisitsDeleterInterface
     ) {
     }
 
+    /**
+     * @throws ShortUrlNotFoundException
+     */
     public function deleteShortUrlVisits(ShortUrlIdentifier $identifier, ?ApiKey $apiKey): BulkDeleteResult
     {
         $this->resolver->resolveShortUrl($identifier, $apiKey);
--- module/Core/src/ShortUrl/ShortUrlVisitsDeleterInterface.php
+++ module/Core/src/ShortUrl/ShortUrlVisitsDeleterInterface.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace Shlinkio\Shlink\Core\ShortUrl;
 
+use Shlinkio\Shlink\Core\Exception\ShortUrlNotFoundException;
 use Shlinkio\Shlink\Core\Model\BulkDeleteResult;
 use Shlinkio\Shlink\Core\ShortUrl\Model\ShortUrlIdentifier;
 use Shlinkio\Shlink\Rest\Entity\ApiKey;
 
 interface ShortUrlVisitsDeleterInterface
 {
+    /**
+     * @throws ShortUrlNotFoundException
+     */
     public function deleteShortUrlVisits(ShortUrlIdentifier $identifier, ?ApiKey $apiKey): BulkDeleteResult;
 }
