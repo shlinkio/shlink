794d926e3a260e20bbe5988cb54269fc73c714cc
Alejandro Celaya <alejandrocelaya@gmail.com>
2023-05-21 14:30:20 +0200

    Update docker entry point to use new shlink-installer init command

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,28 +1,16 @@
 #!/usr/bin/env sh
 set -e
 
-# If SHELL_VERBOSITY was not explicitly provided, run commands in quite mode (-q)
-[ $SHELL_VERBOSITY ] && flags="" || flags="-q"
-
 cd /etc/shlink
 
-echo "Creating fresh database if needed..."
-php bin/cli db:create -n ${flags}
+flags="--clear-db-cache"
 
-echo "Updating database..."
-php bin/cli db:migrate -n ${flags}
-
-echo "Generating proxies..."
-php bin/doctrine orm:generate-proxies -n ${flags}
-
-echo "Clearing entities cache..."
-php bin/doctrine orm:clear-cache:metadata -n ${flags}
-
-# Try to download GeoLite2 db file only if the license key env var was defined and skipping was not explicitly set
-if [ ! -z "${GEOLITE_LICENSE_KEY}" ] && [ "${SKIP_INITIAL_GEOLITE_DOWNLOAD}" != "true" ]; then
-  echo "Downloading GeoLite2 db file..."
-  php bin/cli visit:download-db -n ${flags}
+# Skip downloading GeoLite2 db file if the license key env var was not defined or skipping was explicitly set
+if [ -z "${GEOLITE_LICENSE_KEY}" ] || [ "${SKIP_INITIAL_GEOLITE_DOWNLOAD}" == "true" ]; then
+  flags="${flags} --skip-download-geolite"
 fi
+
+php vendor/bin/shlink-installer init ${flags}
 
 # Periodically run visit:locate every hour, if ENABLE_PERIODIC_VISIT_LOCATE=true was provided and running as root
 if [ "${ENABLE_PERIODIC_VISIT_LOCATE}" = "true" ] && [ "${SHLINK_USER_ID}" = "root" ]; then
