b145d106b030fa2828f02660888719df38c9d695
Alejandro Celaya <alejandrocelaya@gmail.com>
2023-06-08 18:41:36 +0200

    Add matomo env vars and config

---
+++ config/autoload/matomo.global.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use Shlinkio\Shlink\Core\Config\EnvVars;
+
+return [
+
+    'matomo' => [
+        'enabled' => (bool) EnvVars::MATOMO_ENABLED->loadFromEnv(false),
+        'base_url' => EnvVars::MATOMO_BASE_URL->loadFromEnv(),
+        'site_id' => EnvVars::MATOMO_SITE_ID->loadFromEnv(),
+        'token' => EnvVars::MATOMO_TOKEN->loadFromEnv(),
+    ],
+
+];
