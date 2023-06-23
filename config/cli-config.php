1d7c9fd55329e14aff191cfcde8db67392924833
Alejandro Celaya <alejandrocelaya@gmail.com>
2023-06-23 09:16:33 +0200

    Refactor cli-config file as it's currently used by doctrine migrations only

--- config/cli-config.php
+++ config/cli-config.php
@@ -2,11 +2,29 @@
 
 declare(strict_types=1);
 
+use Doctrine\Migrations\Configuration\EntityManager\ExistingEntityManager;
+use Doctrine\Migrations\Configuration\Migration\ConfigurationArray;
+use Doctrine\Migrations\DependencyFactory;
 use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\Tools\Console\ConsoleRunner;
+
+// This file is currently used by docrtrine migrations only
 
 return (static function () {
     /** @var EntityManager $em */
     $em = include __DIR__ . '/entity-manager.php';
-    return ConsoleRunner::createHelperSet($em);
+
+    $migrationsConfig = [
+        'migrations_paths' => [
+            'ShlinkMigrations' => 'data/migrations',
+        ],
+        'table_storage' => [
+            'table_name' => 'migrations',
+        ],
+        'custom_template' => 'data/migrations_template.txt',
+    ];
+
+    return DependencyFactory::fromEntityManager(
+        new ConfigurationArray($migrationsConfig),
+        new ExistingEntityManager($em),
+    );
 })();
