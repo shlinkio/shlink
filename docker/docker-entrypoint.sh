20731c10d052f215c8929fb503b34757084c7c18
Gilberto Miralla-Flores <gmiralla@kennesaw.edu>
2023-09-20 13:51:57 -0400

    Support for reading passwords from _FILE suffixed env vars

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,6 +1,32 @@
 #!/usr/bin/env sh
 set -e
 
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+# Read passwords from files for supported env vars with _FILE suffix
+file_env 'DB_PASSWORD' ''
+file_env 'RABBITMQ_PASSWORD' ''
+
 cd /etc/shlink
 
 flags="--no-interaction --clear-db-cache"
