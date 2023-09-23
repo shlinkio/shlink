ba4a66f772e740e6c5fcf73fc61d2b56d5e9d6f8
Alejandro Celaya <alejandrocelaya@gmail.com>
2023-09-23 08:16:00 +0200

    Add InitialApiKeyCommand unit test

--- module/Rest/test/Service/ApiKeyServiceTest.php
+++ module/Rest/test/Service/ApiKeyServiceTest.php
@@ -6,7 +6,6 @@ namespace ShlinkioTest\Shlink\Rest\Service;
 
 use Cake\Chronos\Chronos;
 use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\EntityRepository;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\MockObject\MockObject;
