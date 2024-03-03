c346fd06022737c742d32f8b9b291f8ecf02a331
Alejandro Celaya <alejandrocelaya@gmail.com>
2024-03-03 10:10:39 +0100

    Reduce duplicated code when parsing short codes and domains from CLI

--- module/CLI/src/Command/RedirectRule/ManageRedirectRulesCommand.php
+++ module/CLI/src/Command/RedirectRule/ManageRedirectRulesCommand.php
@@ -4,16 +4,14 @@ declare(strict_types=1);
 
 namespace Shlinkio\Shlink\CLI\Command\RedirectRule;
 
+use Shlinkio\Shlink\CLI\Input\ShortUrlIdentifierInput;
 use Shlinkio\Shlink\CLI\RedirectRule\RedirectRuleHandlerInterface;
 use Shlinkio\Shlink\CLI\Util\ExitCode;
 use Shlinkio\Shlink\Core\Exception\ShortUrlNotFoundException;
 use Shlinkio\Shlink\Core\RedirectRule\ShortUrlRedirectRuleServiceInterface;
-use Shlinkio\Shlink\Core\ShortUrl\Model\ShortUrlIdentifier;
 use Shlinkio\Shlink\Core\ShortUrl\ShortUrlResolverInterface;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -23,27 +21,32 @@ class ManageRedirectRulesCommand extends Command
 {
     public const NAME = 'short-url:manage-rules';
 
+    private readonly ShortUrlIdentifierInput $shortUrlIdentifierInput;
+
     public function __construct(
         protected readonly ShortUrlResolverInterface $shortUrlResolver,
         protected readonly ShortUrlRedirectRuleServiceInterface $ruleService,
         protected readonly RedirectRuleHandlerInterface $ruleHandler,
     ) {
         parent::__construct();
+        $this->shortUrlIdentifierInput = new ShortUrlIdentifierInput(
+            $this,
+            shortCodeDesc: 'The short code which rules we want to set.',
+            domainDesc: 'The domain for the short code.',
+        );
     }
 
     protected function configure(): void
     {
         $this
             ->setName(self::NAME)
-            ->setDescription('Set redirect rules for a short URL')
-            ->addArgument('shortCode', InputArgument::REQUIRED, 'The short code which rules we want to set.')
-            ->addOption('domain', 'd', InputOption::VALUE_REQUIRED, 'The domain for the short code.');
+            ->setDescription('Set redirect rules for a short URL');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $identifier = ShortUrlIdentifier::fromCli($input);
+        $identifier = $this->shortUrlIdentifierInput->toShortUrlIdentifier($input);
 
         try {
             $shortUrl = $this->shortUrlResolver->resolveShortUrl($identifier);
