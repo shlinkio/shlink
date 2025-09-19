020f7ca4ed28dec3c5e296625c0451b0a3513708
Viacheslav Shcherbyna <vshcherbyna@neklo.com>
2025-09-19 12:56:27 +0300

    SWR-19847 #comment Short URL: Permissions Error

--- module/Core/src/Action/RedirectAction.php
+++ module/Core/src/Action/RedirectAction.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Shlinkio\Shlink\Core\Action;
 
 use Fig\Http\Message\StatusCodeInterface;
+use League\Uri\Uri;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface;
 use Shlinkio\Shlink\Core\ShortUrl\Entity\ShortUrl;
@@ -12,6 +13,7 @@ use Shlinkio\Shlink\Core\ShortUrl\Helper\ShortUrlRedirectionBuilderInterface;
 use Shlinkio\Shlink\Core\ShortUrl\ShortUrlResolverInterface;
 use Shlinkio\Shlink\Core\Util\RedirectResponseHelperInterface;
 use Shlinkio\Shlink\Core\Visit\RequestTrackerInterface;
+use const Shlinkio\Shlink\SENSITIVE_HOSTS;
 
 class RedirectAction extends AbstractTrackingAction implements StatusCodeInterface
 {
@@ -26,7 +28,23 @@ class RedirectAction extends AbstractTrackingAction implements StatusCodeInterfa
 
     protected function createSuccessResp(ShortUrl $shortUrl, ServerRequestInterface $request): Response
     {
-        $longUrl = $this->redirectionBuilder->buildShortUrlRedirect($shortUrl, $request->getQueryParams());
+        $queryParams = (false === $this->isSensitiveHost($shortUrl))
+            ? $request->getQueryParams()
+            : [];
+
+        $longUrl = $this->redirectionBuilder->buildShortUrlRedirect($shortUrl, $queryParams);
+
         return $this->redirectResponseHelper->buildRedirectResponse($longUrl);
+    }
+
+    /**
+     * @param ShortUrl $shortUrl
+     * @return bool
+     */
+    private function isSensitiveHost(ShortUrl $shortUrl): bool
+    {
+        $uri = Uri::createFromString($shortUrl->getLongUrl());
+
+        return in_array($uri->getHost(), SENSITIVE_HOSTS, true);
     }
 }
