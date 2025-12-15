635e968bb2bbbe72a2fb474fa6fddfde4d69d067
Alejandro Celaya <alejandrocelaya@gmail.com>
2025-12-15 09:35:38 +0100

    Convert EditShortUrlCommand into invokable command

--- module/CLI/src/Command/ShortUrl/EditShortUrlCommand.php
+++ module/CLI/src/Command/ShortUrl/EditShortUrlCommand.php
@@ -4,55 +4,49 @@ declare(strict_types=1);
 
 namespace Shlinkio\Shlink\CLI\Command\ShortUrl;
 
-use Shlinkio\Shlink\CLI\Input\ShortUrlDataInput;
-use Shlinkio\Shlink\CLI\Input\ShortUrlIdentifierInput;
+use Shlinkio\Shlink\CLI\Command\ShortUrl\Input\ShortUrlDataInput;
 use Shlinkio\Shlink\Core\Exception\ShortUrlNotFoundException;
 use Shlinkio\Shlink\Core\ShortUrl\Helper\ShortUrlStringifierInterface;
+use Shlinkio\Shlink\Core\ShortUrl\Model\ShortUrlEdition;
+use Shlinkio\Shlink\Core\ShortUrl\Model\ShortUrlIdentifier;
 use Shlinkio\Shlink\Core\ShortUrl\ShortUrlServiceInterface;
+use Symfony\Component\Console\Attribute\Argument;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Attribute\MapInput;
+use Symfony\Component\Console\Attribute\Option;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 use function sprintf;
 
+#[AsCommand(
+    name: EditShortUrlCommand::NAME,
+    description: 'Edit an existing short URL',
+)]
 class EditShortUrlCommand extends Command
 {
     public const string NAME = 'short-url:edit';
-
-    private readonly ShortUrlDataInput $shortUrlDataInput;
-    private readonly ShortUrlIdentifierInput $shortUrlIdentifierInput;
 
     public function __construct(
         private readonly ShortUrlServiceInterface $shortUrlService,
         private readonly ShortUrlStringifierInterface $stringifier,
     ) {
         parent::__construct();
-
-        $this->shortUrlDataInput = new ShortUrlDataInput($this, longUrlAsOption: true);
-        $this->shortUrlIdentifierInput = new ShortUrlIdentifierInput(
-            $this,
-            shortCodeDesc: 'The short code to edit',
-            domainDesc: 'The domain to which the short URL is attached.',
-        );
     }
 
-    protected function configure(): void
-    {
-        $this
-            ->setName(self::NAME)
-            ->setDescription('Edit an existing short URL');
-    }
-
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
-        $io = new SymfonyStyle($input, $output);
-        $identifier = $this->shortUrlIdentifierInput->toShortUrlIdentifier($input);
+    public function __invoke(
+        SymfonyStyle $io,
+        #[Argument('The short code to edit')] string $shortCode,
+        #[MapInput] ShortUrlDataInput $data,
+        #[Option('The domain to which the short URL is attached', shortcut: 'd')] string|null $domain = null,
+        #[Option('The long URL to set', shortcut: 'l')] string|null $longUrl = null,
+    ): int {
+        $identifier = ShortUrlIdentifier::fromShortCodeAndDomain($shortCode, $domain);
 
         try {
             $shortUrl = $this->shortUrlService->updateShortUrl(
                 $identifier,
-                $this->shortUrlDataInput->toShortUrlEdition($input),
+                ShortUrlEdition::fromRawData($data->toArray()),
             );
 
             $io->success(sprintf('Short URL "%s" properly edited', $this->stringifier->stringify($shortUrl)));
